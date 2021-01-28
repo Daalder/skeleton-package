b1d9836fd7ebf377f2e010d19d13c6087b44dda4
Egbert Wietses <egbertwietses@MacBook-Air.localdomain>
2021-01-28 12:58:09 +0100

    Add test example

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Daalder\SkeletonPackage\Tests;
+
+/**
+ * Class ExampleTest
+ * @package Daalder\SkeletonPackage\Tests
+ */
+class ExampleTest extends TestCase
+{
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
